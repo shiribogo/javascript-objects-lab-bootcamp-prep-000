29cdbd63bc28fed736309e0202623d547b2788eb
Shiri Mund <shiribogo@gmail.com>
2018-04-15 19:50:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function updateObjectWithKeyAndValue(object, key, value)
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign 
+}
